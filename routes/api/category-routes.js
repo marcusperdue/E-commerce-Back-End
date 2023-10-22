db5ee08f8f80e6d809ed6d53ad9a4b1e2ffa7baa
Marcus Perdue <marcusperduee@gmail.com>
2023-10-22 11:58:14 -0700

    added route to update a category by its 'id' value

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -33,6 +33,7 @@ router.get('/:id', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
 // Route to create a new category
 router.post('/', async (req, res) => {
   try {
@@ -48,8 +49,24 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+// Route to update a category by its 'id' value
+router.put('/:id', async (req, res) => {
+  try {
+    const categoryId = req.params.id;
+    const updatedCategoryData = await Category.update(req.body, {
+      where: { id: categoryId },
+    });
+
+    if (updatedCategoryData[0] === 0) {
+      res.status(404).json({ message: 'Category not found' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Category updated successfully' });
+  } catch (err) {
+    console.error(err);
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
